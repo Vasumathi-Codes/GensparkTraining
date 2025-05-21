ccbced7bb06c08319b917fa25a4dc6212e6d664f
Vasumathi-Codes <vasumathi.connect@gmail.com>
2025-05-21 18:53:07 +0530

    Added Day-13 FN-Session code

---
+++ Day - 13 (21-05-2025)/FN-Session-EmployeeManagement/CSharp_Day3/Interfaces/IEmployeeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp_Day3.Models;
+
+namespace CSharp_Day3.Interfaces
+{
+    public interface IEmployeeService
+    {
+        int AddEmployee(Employee employee);
+        List<Employee>? SearchEmployee(SearchModel searchModel);
+
+        bool UpdateEmployee(Employee employee);
+    }
+}
---
+++ Day - 13 (21-05-2025)/FN-Session-EmployeeManagement/CSharp_Day3/Program.cs
@@ -0,0 +1,17 @@
+﻿using CSharp_Day3.Models;
+using CSharp_Day3.Repositories;
+using CSharp_Day3.Services;
+
+namespace CSharp_Day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeRepository = new EmployeeRepository();
+            var employeeService = new EmployeeService(employeeRepository);
+            ManageEmployee manageEmployee = new ManageEmployee(employeeService);
+            manageEmployee.Start();
+        }
+    }
+}
